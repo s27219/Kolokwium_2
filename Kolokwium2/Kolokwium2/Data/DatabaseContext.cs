b90a4e60f0a9f3e7365f8ad07d79d06232198c3d
s27219 <27219@pjwstk.edu.pl>
2024-06-11 12:12:31 +0200

    add project

---
+++ Kolokwium2/Kolokwium2/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Kolokwium2.Data;
+
+public class DatabaseContext : DbContext
+{
+    protected DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    //...
+}
